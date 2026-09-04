f8edbcf17e9b90e6f968ac6632b5c280f12dff23
Yu Ting <2605759@sit.singaporetech.edu.sg>
2026-09-04 15:30:17 +0800

    Updated followers

--- hello.py
+++ hello.py
@@ -7,6 +7,15 @@ username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
 
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
