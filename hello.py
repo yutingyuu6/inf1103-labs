9b804cdbd3588e7110b6ab8b4fc50fe8bff95430
Yu Ting <2605759@sit.singaporetech.edu.sg>
2026-09-04 15:25:05 +0800

    Updated profile's username, bio and followers

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("=============================")
 print("Welcome here")
 print("My first post!")
 print("=============================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
