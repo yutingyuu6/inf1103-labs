61118e93c4e5f45e40d6f1e8410ad3019a315629
Yu Ting <2605759@sit.singaporetech.edu.sg>
2026-09-22 00:03:57 +0800

    Added modular_auditor.py

---
+++ modular_auditor.py
@@ -0,0 +1,47 @@
+# Smart Inventory Auditor
+# Initialize inventory to zero
+inventory = 0
+# Variable for number of failed/rejected entries
+failed_entries = 0
+
+# While loop
+while True:
+    # User input for stock quantity
+    stock_input = input("Enter stock quantity: (or type 'quit' to exit)")
+
+    # Changes stock_input to lowercase
+    # If user enters 'quit', the while loop will break
+    if stock_input.lower() == 'quit':
+        print("Quitting the program.")
+        break
+
+    # Rejects inputs which are not integers
+    elif not stock_input.isdigit():
+        failed_entries += 1
+        print("Error: Please enter a valid stock quantity or type 'quit' to exit.")
+        continue
+
+    # Reject negative numbers
+    elif int(stock_input) < 0:
+        failed_entries += 1
+        print("Error: Stock quantity cannot be negative.")
+        continue
+
+    else:
+        # Update inventory
+        inventory += int(stock_input)
+
+        # If inventory exceeds 500, an alert will be printed
+        # The while loop will break
+        if inventory > 500:
+            print("Alert: Total inventory exceeded 500!")
+            break
+        
+        else:
+            print("Inventory updated")
+            # Print current inventory total
+            print("Inventory total:", inventory)
+
+# After the while loop breaks, Total Units Processed and Number of Failed/Rejected Entries are printed
+print("Total Units Processed: ", inventory)
+print("Number of Failed/Rejected Entries: ", failed_entries)
