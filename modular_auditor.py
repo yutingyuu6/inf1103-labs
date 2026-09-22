a19ec88727adfc177fa64f86233236c568a09825
Yu Ting <2605759@sit.singaporetech.edu.sg>
2026-09-22 16:21:54 +0800

    Added get_valid_input() function and updated while loop

--- modular_auditor.py
+++ modular_auditor.py
@@ -4,27 +4,40 @@ inventory = 0
 # Variable for number of failed/rejected entries
 failed_entries = 0
 
-# While loop
-while True:
+# get_valid_input() function
+def get_valid_input():
     # User input for stock quantity
     stock_input = input("Enter stock quantity: (or type 'quit' to exit)")
 
     # Changes stock_input to lowercase
-    # If user enters 'quit', the while loop will break
     if stock_input.lower() == 'quit':
+        return "quit"
+    
+    # Rejects inputs which are not integers
+    elif not stock_input.isdigit():
+        return None
+    
+    # Reject negative numbers
+    elif int(stock_input) < 0:
+        return None
+
+    # Returns valid stock input
+    else:
+        return int(stock_input)
+
+# While loop
+while True:
+    # Call get_valid_input() function
+    stock_input = get_valid_input()
+
+    # If user enters 'quit', the while loop will break
+    if stock_input == "quit":
         print("Quitting the program.")
         break
 
-    # Rejects inputs which are not integers
-    elif not stock_input.isdigit():
+    elif stock_input is None:
         failed_entries += 1
         print("Error: Please enter a valid stock quantity or type 'quit' to exit.")
-        continue
-
-    # Reject negative numbers
-    elif int(stock_input) < 0:
-        failed_entries += 1
-        print("Error: Stock quantity cannot be negative.")
         continue
 
     else:
@@ -36,7 +49,7 @@ while True:
         if inventory > 500:
             print("Alert: Total inventory exceeded 500!")
             break
-        
+            
         else:
             print("Inventory updated")
             # Print current inventory total
